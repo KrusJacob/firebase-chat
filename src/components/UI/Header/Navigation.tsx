b47abcb6c6f39a6a8ed81b656371246c46c5cfe5
Jacob Krus <coneter@mail.ru>
2025-02-14 21:06:49 +0300

    add hamburger-menu

--- src/components/UI/Header/Navigation.tsx
+++ src/components/UI/Header/Navigation.tsx
@@ -1,20 +1,69 @@
-import { Box, IconButton, Link } from "@mui/material";
+import { Box, Drawer, IconButton, Link, List, ListItem, ListItemText, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router";
+import { useState } from "react";
 
+const navigateLinks = [
+  { name: "Home", path: "/" },
+  { name: "Chat", path: "/chat" },
+];
 const Navigation = () => {
   const navigate = useNavigate();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => () => {
+    setIsDrawerOpen(open);
+  };
+
+  const drawer = (
+    <Box
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+      sx={{ width: 250, py: 2, bgcolor: "#94ced8", height: "100%" }}
+    >
+      <List>
+        {navigateLinks.map((link) => (
+          <ListItem
+            sx={{ cursor: "pointer", justifyContent: "center", ":hover": { bgcolor: "#ffffff" } }}
+            component={"li"}
+            key={link.name}
+            onClick={() => navigate(link.path)}
+          >
+            <Typography fontSize={24}>{link.name}</Typography>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <Box sx={{ flexGrow: 1 }} display={"flex"} alignItems={"center"} gap={4}>
-      <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+      <IconButton
+        onClick={toggleDrawer(true)}
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2, display: { xs: "block", md: "none" } }}
+      >
         <MenuIcon />
       </IconButton>
-      <Link onClick={() => navigate("/")} color="inherit" fontSize={24} underline="none" href="#">
-        Home
-      </Link>
-      <Link onClick={() => navigate("/chat")} color="inherit" fontSize={24} underline="none" href="#">
-        Chat
-      </Link>
+      {navigateLinks.map((link) => (
+        <Link
+          sx={{ display: { xs: "none", md: "block", cursor: "pointer" } }}
+          key={link.name}
+          onClick={() => navigate(link.path)}
+          color="inherit"
+          fontSize={24}
+          underline="none"
+        >
+          {link.name}
+        </Link>
+      ))}
+
+      <Drawer anchor="left" open={isDrawerOpen} onClose={toggleDrawer(false)}>
+        {drawer}
+      </Drawer>
     </Box>
   );
 };
